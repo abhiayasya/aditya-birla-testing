0f150ed75149e629c20e04d3679f43789f1c2ff6
Abhishek Rajput <168184952+abhiayasya@users.noreply.github.com>
2025-02-12 12:34:41 +0530

    ui update

--- src/components/MainContainer/MainContainer.jsx
+++ src/components/MainContainer/MainContainer.jsx
@@ -2,9 +2,9 @@ export const MainContainer = (props) => {
     const { children, className } = props;
     return (
       <div
-        className={`max-w-[1440px] mx-auto lg:px-20 md:px-10 px-4 md:mb-10 py-10 ${className}`}
+        className={` mx-auto lg:px-20 md:px-10 px-4 md:mb-10 py-10 ${className}`}
       >
-        <div className="">{children}</div>
+        <div className="max-w-[1280px] mx-auto">{children}</div>
       </div>
     );
   };
